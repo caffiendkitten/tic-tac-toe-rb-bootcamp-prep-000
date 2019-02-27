b6a3cf078628679f070e9bf5236f2b5d7b7fe113
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-02-27 05:58:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -111,3 +111,16 @@ def won?(board)
   }
   return false
 end
+
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
